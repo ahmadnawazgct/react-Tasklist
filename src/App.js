82229b26a731ed1b29885b153d0221e5c852ac89
Nawaz Ahmad <ahmadnawazgct@gmail.com>
2019-05-09 02:18:37 -0700

    file change

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import uuid from "uuid";
 import TodoList from './components/TodoList';
 import TodoInput from './components/TodoInput';
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <TodoInput />
-        <TodoList />
+ class App extends Component {
+  state = {
+    items: [{ id: 1, title: "wake up" }, { id: 2, title: "make breakfast" }],
+    id:uuid()
+  };
+  render(){
+    console.log(this.state);
+    return (
+      <div className="container">
+        <div className="row">
+          <TodoInput />
+          <TodoList />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 export default class TodoItem extends Component{
     render(){
         return(
-            <div>
+            <p>
                 Hello from Todo Item
-            </div>
+            </p>
         )
     }
 }
